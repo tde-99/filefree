2643df19e52b01a723a73896b589a9c2bd47c9f9
tde-99 <deltaentertainmentofc@gmail.com>
2026-02-18 22:28:09 +0530

    Update config.py

---
+++ config.py
@@ -0,0 +1,46 @@
+# Telegram Bot
+TG_BOT_TOKEN=8163907060:AAEbMLWLiLW-MgLlqrw041OdYvKP0TQXEkQ
+APP_ID=9698652
+API_HASH=b354710ab18b84e00b65c62ba7a9c043
+CHANNEL_ID=-1002568581749
+OWNER_ID=7678562257
+PORT=3435
+
+# Database
+DATABASE_URL=mongodb+srv://obito:umaid2008@cluster0.engyc.mongodb.net/?retryWrites=true&w=majority
+DATABASE_NAME=orion
+
+# Bot Settings
+MIN_ID=1
+MAX_ID=150
+IS_VERIFY=false
+TUT_VID=https://t.me/delight_link/2
+TG_BOT_WORKERS=200
+
+# Images
+START_PIC=https://telegra.ph/file/ec17880d61180d3312d6a.jpg
+FORCE_PIC=https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg
+QR_PIC=https://envs.sh/B7w.png
+PICS=https://envs.sh/4Iq.jpg https://envs.sh/4IW.jpg https://envs.sh/4IB.jpg https://envs.sh/4In.jpg
+
+# Caption
+CUSTOM_CAPTION=<b>ʙʏ @Javpostr</b>
+DISABLE_CHANNEL_BUTTON=True
+
+# Premium
+OWNER_TAG=rohit_1888
+UPI_ID=rohit23pnb@axl
+UPI_IMAGE_URL=https://t.me/paymentbot6/2
+SCREENSHOT_URL=t.me/rohit_1888
+PRICE1=0 rs
+PRICE2=60 rs
+PRICE3=150 rs
+PRICE4=280 rs
+PRICE5=550 rs
+
+# Referral
+REFERRAL_COUNT=5
+REFERRAL_PREMIUM_DAYS=7
+
+# User Reply
+USER_REPLY_TEXT=⚠️ Pʟᴇᴀsᴇ ᴜsᴇ ᴛʜᴇ ᴘʀᴏᴘᴇʀ ᴄᴏᴍᴍᴀɴᴅs ᴏʀ ʙᴜᴛᴛᴏɴs ᴛᴏ ɪɴᴛᴇʀᴀᴄᴛ ᴡɪᴛʜ ᴛʜᴇ ʙᴏᴛ.\n\nUse /help to see available commands.
